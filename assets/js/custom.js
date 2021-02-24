cf59f7d69e2e9f19a6d49acea55fecdd32eaf726
Prince kumar singh <princesinghpnjr@gmail.com>
2021-02-25 00:46:38 +0530

    multiple update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,173 @@
-(function($){'use strict';jQuery('.mean-menu').meanmenu({meanScreenWidth:"991"});$(function(){$('body').addClass('loaded');});$('select').niceSelect();$(window).on('scroll',function(){if($(this).scrollTop()>150){$('.navbar-area').addClass("is-sticky");}
-else{$('.navbar-area').removeClass("is-sticky");}});$('.hero-slider-wrap').owlCarousel({loop:true,margin:0,nav:true,mouseDrag:true,items:1,dots:false,autoHeight:true,autoplay:true,smartSpeed:800,autoplayHoverPause:true,navText:["<i class='bx bx-chevrons-left'></i>","<i class='bx bx-chevrons-right'></i>",],});$('.partner-wrap').owlCarousel({loop:true,nav:false,autoplay:true,autoplayHoverPause:true,mouseDrag:true,margin:0,center:false,dots:false,smartSpeed:1500,responsive:{0:{items:2},576:{items:3},768:{items:4},992:{items:5},1200:{items:5}}});$('.client-wrap').owlCarousel({loop:true,margin:30,nav:false,mouseDrag:true,items:1,dots:false,autoHeight:true,autoplay:true,smartSpeed:1500,autoplayHoverPause:true,center:true,responsive:{0:{items:1,},576:{items:1,},768:{items:2,},992:{items:3,},1200:{items:3,}}});$('.single-security-wrap').owlCarousel({loop:true,margin:30,nav:false,mouseDrag:true,items:1,dots:true,autoHeight:true,autoplay:true,smartSpeed:1500,autoplayHoverPause:true,center:false,responsive:{0:{items:1,},576:{items:2,},768:{items:2,},992:{items:3,},1200:{items:4,}}});$('.solutions-wrap').owlCarousel({loop:true,margin:30,nav:false,mouseDrag:true,items:1,dots:false,autoHeight:true,autoplay:true,smartSpeed:1500,autoplayHoverPause:true,center:true,responsive:{0:{items:1,},576:{items:1,},768:{items:2,},992:{items:3,},1200:{items:3,}}});$('.odometer').appear(function(e){var odo=$(".odometer");odo.each(function(){var countNumber=$(this).attr("data-count");$(this).html(countNumber);});});$(window).on('scroll',function(){var scrolled=$(window).scrollTop();if(scrolled>300)$('.go-top').addClass('active');if(scrolled<300)$('.go-top').removeClass('active');});$('.go-top').on('click',function(){$("html, body").animate({scrollTop:"0"},500);});$('.accordion').find('.accordion-title').on('click',function(){$(this).toggleClass('active');$(this).next().slideToggle('fast');$('.accordion-content').not($(this).next()).slideUp('fast');$('.accordion-title').not($(this)).removeClass('active');});function makeTimer(){var endTime=new Date("november  30, 2021 17:00:00 PDT");var endTime=(Date.parse(endTime))/1000;var now=new Date();var now=(Date.parse(now)/1000);var timeLeft=endTime-now;var days=Math.floor(timeLeft/86400);var hours=Math.floor((timeLeft-(days*86400))/3600);var minutes=Math.floor((timeLeft-(days*86400)-(hours*3600))/60);var seconds=Math.floor((timeLeft-(days*86400)-(hours*3600)-(minutes*60)));if(hours<"10"){hours="0"+hours;}
+(function($)
+{'use strict';
+jQuery('.mean-menu').meanmenu({meanScreenWidth:"991"});
+$(function()
+{
+    $('body').addClass('loaded');
+}
+);
+
+$('select').niceSelect();
+$(window).on('scroll',function()
+{
+    if($(this).scrollTop()>150){$('.navbar-area').addClass("is-sticky");
+}
+    else{
+    $('.navbar-area').removeClass("is-sticky");
+}
+}
+);
+$('.hero-slider-wrap').owlCarousel(
+    {
+        loop:true,
+        margin:0,
+        nav:true,
+        mouseDrag:true,
+        items:1,
+        dots:false,
+        autoHeight:true,
+        autoplay:true,
+        smartSpeed:800,
+        autoplayHoverPause:true,
+        navText:["<i class='bx bx-chevrons-left'></i>","<i class='bx bx-chevrons-right'></i>",],});
+        
+        $('.partner-wrap').owlCarousel(
+            {
+                loop:true,
+                nav:false,
+                autoplay:true,
+                autoplayHoverPause:true,
+                mouseDrag:true,
+                margin:0,
+                center:false,
+                dots:false,
+                smartSpeed:1500,
+                responsive:{
+                    0:{
+                        items:2
+                    },
+                    576:{
+                        items:3
+                    },
+                    768:{
+                        items:4
+                    },
+                    992:{
+                        items:5
+                    },
+                    1200:{
+                        items:5
+                    }}});
+                    $('.client-wrap').owlCarousel({
+                        loop:true,
+                        margin:30,
+                        nav:false,
+                        mouseDrag:true,
+                        items:1,
+                        dots:false,
+                        autoHeight:true,
+                        autoplay:true,
+                        smartSpeed:1500,
+                        autoplayHoverPause:true,
+                        center:true,
+                        responsive:{
+                            0:{
+                                items:1,
+                            },
+                            576:{
+                                items:1,
+                            }
+                            ,768:{
+                                items:2,
+                            },
+                            992:{
+                                items:3,
+                            },
+                            1200:{
+                                items:3,
+                            }}});
+                            $('.single-security-wrap').owlCarousel({
+                                loop:true,
+                                margin:30,
+                                nav:false,
+                                mouseDrag:true,
+                                items:1,
+                                dots:true,
+                                autoHeight:true,
+                                autoplay:true,
+                                smartSpeed:1500,
+                                autoplayHoverPause:true,
+                                center:false,
+                                responsive:{
+                                    0:{
+                                        items:1,
+                                    }
+                                    ,576:{
+                                        items:2,
+                                    },
+                                    768:{
+                                        items:2,
+                                    },
+                                    992:{
+                                        items:3,
+                                    },
+                                    1200:{
+                                        items:4,
+                                    }}});
+                                    $('.solutions-wrap').owlCarousel({
+                                        loop:true,
+                                        margin:30,
+                                        nav:false,
+                                        mouseDrag:true,
+                                        items:1,
+                                        dots:false,
+                                        autoHeight:true,
+                                        autoplay:true,
+                                        smartSpeed:1500,
+                                        autoplayHoverPause:true,
+                                        center:true,
+                                        responsive:{
+                                            0:{
+                                                items:1,
+                                            },
+                                            576:{
+                                                items:1,
+                                            },
+                                            768:{
+                                                items:1,
+                                            },
+                                            992:{
+                                                items:1,
+                                            },
+                                            1200:{
+                                                items:1,
+                                            }}});
+                                            $('.odometer').appear(function(e){
+                                                var odo=$(".odometer");
+                                                odo.each(function(){
+                                                    var countNumber=$(this).attr("data-count");
+                                                    $(this).html(countNumber);
+                                                }
+                                                );
+                                            });
+                                            $(window).on('scroll',function(){
+                                                var scrolled=$(window).scrollTop();
+                                                if(scrolled>300)$('.go-top').addClass('active');
+                                                if(scrolled<300)$('.go-top').removeClass('active');
+                                            });
+                                            $('.go-top').on('click',function(){
+                                                $("html, body").animate({
+                                                    scrollTop:"0"},
+                                                    500);
+                                                });
+                                                $('.accordion').find('.accordion-title').on('click',function(){
+                                                    $(this).toggleClass('active');
+                                                    $(this).next().slideToggle('fast');
+                                                    $('.accordion-content').not($(this).next()).slideUp('fast');
+                                                    $('.accordion-title').not($(this)).removeClass('active');
+                                                });
+                                                function makeTimer(){var endTime=new Date("november  30, 2021 17:00:00 PDT");
+                                                var endTime=(Date.parse(endTime))/1000;var now=new Date();var now=(Date.parse(now)/1000);var timeLeft=endTime-now;var days=Math.floor(timeLeft/86400);var hours=Math.floor((timeLeft-(days*86400))/3600);var minutes=Math.floor((timeLeft-(days*86400)-(hours*3600))/60);var seconds=Math.floor((timeLeft-(days*86400)-(hours*3600)-(minutes*60)));if(hours<"10"){hours="0"+hours;}
 if(minutes<"10"){minutes="0"+minutes;}
 if(seconds<"10"){seconds="0"+seconds;}
 $("#days").html(days+"<span>Days</span>");$("#hours").html(hours+"<span>Hours</span>");$("#minutes").html(minutes+"<span>Minutes</span>");$("#seconds").html(seconds+"<span>Seconds</span>");}
